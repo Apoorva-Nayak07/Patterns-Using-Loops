b9ba8484e9a7417cba834bfad047de781fc2a43b
Apoorva-Nayak07 <nayakapoorva343@gmail.com>
2024-12-08 07:11:44 +0530

    initial commit

---
+++ Apoorva-workspace/PATTERN/src/Patterns/PatternsUsingLoops.java
@@ -0,0 +1,18 @@
+package Patterns;
+public class PatternsUsingLoops {
+	      public static void main(String[] args) {
+					int i=0, j=0;
+					while(i<2)
+					{
+					j=0;
+					
+					while(j<10)
+					{
+						System.out.print("*");
+						j++;
+					}
+					System.out.println();
+					i++;
+				}
+			}
+		}
