7852b3b70359e6824c79459309c4785ddd84591b
Apoorva-Nayak07 <nayakapoorva343@gmail.com>
2024-12-08 10:33:10 +0530

    initial commit

---
+++ Apoorva-workspace/Patterns/src/Pattern/PatternUsingLoop.java
@@ -0,0 +1,17 @@
+package Pattern;
+public class PatternUsingLoop {
+		public static void main(String[]args) {
+			int i = 0,	j=0;
+			while(i<2)
+			{
+				j=0;
+				while(j<10)
+				{
+					System.out.print("*");
+					j++;
+				}
+				i++;
+		        System.out.println();
+	}
+}
+}
--- Apoorva-workspace/Patterns/src/Pattern1/pattern.java
+++ Apoorva-workspace/Patterns/src/Pattern1/pattern.java
@@ -1,10 +1,18 @@
 package Pattern1;
-
 public class pattern {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+      public static void main(String[] args) {
+				int i=0, j=0;
+				while(i<2)
+				{
+				j=0;
+				
+				while(j<10)
+				{
+					System.out.print("*");
+					j++;
+				}
+				System.out.println();
+				i++;
+			}
+		}
 	}
-
-}
