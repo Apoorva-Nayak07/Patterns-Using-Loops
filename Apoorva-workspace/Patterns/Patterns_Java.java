f5369b123c9f6244f245b85a17b0c47a85fbf4a2
Apoorva-Nayak07 <nayakapoorva343@gmail.com>
2024-12-08 06:51:45 +0530

    initial commit

---
+++ Apoorva-workspace/Patterns/Patterns_Java.java
@@ -0,0 +1,18 @@
+package Patters;
+public class Patterns_Java {
+		public static void main(String[] args) {
+			int i=0, j=0;
+			while(i<2)
+			{
+			j=0;
+			
+			while(j<10)
+			{
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
